c0a55873c6669725b3a30098fa7b880a470c51b2
zobirofkir <zobirofkir19@gmail.com>
2024-11-03 12:53:16 +0100

    cretae orders and order_items schema

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'phone',
+        'address',
+        'total_amount',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -57,6 +57,11 @@ class User extends Authenticatable
         return $this->hasMany(Product::class);
     }
     
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+    
     public function generateToken() 
     {
         return $this->createToken('accessToken');
