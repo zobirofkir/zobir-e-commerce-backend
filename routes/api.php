abfcccc32a4ff1be09ca35da5eec189fc5d3e669
zobirofkir <zobirofkir19@gmail.com>
2024-11-04 18:52:38 +0100

    add comments in routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -8,19 +8,48 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Create Register Route
+ */
 Route::post("auth/register", [AuthenticationController::class, 'register']);
+
+/**
+ * Create Login Route
+ */
 Route::post("auth/login", [AuthenticationController::class, 'login']);
+
+/**
+ * Create Logout Route
+ */
 Route::post("auth/logout", [AuthenticationController::class, 'logout']);
 
+/**
+ * Create Authenticated Routes
+ */
 Route::middleware('auth:api')->group(function () {
+    /**
+     * Create Category Routes
+     */
     Route::apiResource('/categories', CategoryController::class);
 
+    /**
+     * Create Product Routes
+     */
     Route::apiResource('/products', ProductController::class);
 
+    /**
+     * Create Order Routes
+     */
     Route::apiResource('/orders', OrderController::class);
 
+    /**
+     * Create Payment Routes
+     */
     Route::post('/orders/{order}/payments', [PaymentController::class, 'processPayment']);
 
+    /**
+     * Create Brand Routes
+     */
     Route::apiResource('/brands', BrandController::class);
 
 });
